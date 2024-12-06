6fe18d5e8c7d3f85a6af6438d0817ee2c007bf2b
Phuong Hoang <phuong.hoangthu09@gmail.com>
2024-12-06 14:06:51 +0700

    update final

--- tlunews/controllers/AdminController.php
+++ tlunews/controllers/AdminController.php
@@ -1,60 +1,65 @@
 <?php
-require_once __DIR__.'/../models/User.php';
-class AdminController{
-    public function index(){
-        include __DIR__.'/../views/Admin/dashboard.php';
+require_once __DIR__ . '/../models/User.php';
+class AdminController
+{
+    public function index()
+    {
+        include __DIR__ . '/../views/Admin/dashboard.php';
     }
     //Login
-    public function login(){
-        include __DIR__.'/../views/Admin/login.php';
+    public function login()
+    {
+        include __DIR__ . '/../views/Admin/login.php';
     }
-    public function handleLogin(){
+    public function handleLogin()
+    {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if(isset($_POST['signIn'])&&$_POST['signIn']==='login'){
+            if (isset($_POST['signIn']) && $_POST['signIn'] === 'login') {
                 if (isset($_POST['username'], $_POST['password'])) {
                     $username = $_POST['username'];
                     $password = $_POST['password'];
-                    $result = User::login($username,$password);
-                    if ($result){
+                    $result = User::login($username, $password);
+                    if ($result) {
                         //Đăng nhập thành công,lưu thông tin người dùng vào sesion
                         session_start();
-                        $_SESSION['user'] = $result['username'];//Lưu thông tin người dùng vào session
+                        $_SESSION['user'] = $result['username']; //Lưu thông tin người dùng vào session
                         // var_dump($_SESSION['user']);
                         // print_r($_SESSION['user']);
                         //Kiểm tra user
-                        if($result['role']==1)//Quản trị viên
+                        if ($result['role'] == 1) //Quản trị viên
                             header("Location: index.php?controller=Admin&action=index");
-                        else header("Location: index.php?controller=News&action=index");
-                    }
-                    else echo "Invalid username or password";
+                        else header("Location: index.php?controller=Home&action=index");
+                    } else echo "Invalid username or password";
                 }
             }
         }
     }
     //Log out
-    public static function logout(){
-        session_start();//Khởi tạo session=>khởi động trước khi thực hiện làm việc với session
+    public static function logout()
+    {
+        session_start(); //Khởi tạo session=>khởi động trước khi thực hiện làm việc với session
         session_destroy();
         header("Location: index.php?controller=Home&action=index");
     }
     //Create
-    public function register(){
-        include __DIR__.'/../views/Admin/register.php';
+    public function register()
+    {
+        include __DIR__ . '/../views/Admin/register.php';
     }
-    public function handleRegister(){
+    public function handleRegister()
+    {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if(isset($_POST['register'])&&$_POST['register']==='register'){
-                if (isset($_POST['username'], $_POST['password'],$_POST['role'])) {
+            if (isset($_POST['register']) && $_POST['register'] === 'register') {
+                if (isset($_POST['username'], $_POST['password'], $_POST['role'])) {
                     $username = $_POST['username'];
                     $password = $_POST['password'];
                     $role = $_POST['role'];
-                    $result = User::create($username,$password,$role);
+                    $result = User::create($username, $password, $role);
                     if ($result)
                         header("Location: index.php?controller=Admin&action=login");
-                    else echo "Register failed";   
+                    else echo "Register failed";
                 }
             }
-        }   
+        }
     }
 }
-?>
--- tlunews/controllers/NewsController.php
+++ tlunews/controllers/NewsController.php
@@ -3,23 +3,24 @@ require_once __DIR__ . '/../models/News.php';
 define('BASE_URL', '/2024CSE485_CongNgheWeb/tlunews');
 class NewsController
 {
-    public function index(){
-        $newsList = News::getAllNewsAdmin();
-        include __DIR__.'/../views/Home/index.php';
-    }
-    public function newsManagement(){
+    public function newsManagement()
+    {
         $news = News::getAllNewsAdmin();
-        include __DIR__.'/../views/Admin/news/index.php';
+        include __DIR__ . '/../views/Admin/news/index.php';
     }
     //Create
-    public function createNews(){
-        include __DIR__.'/../views/Admin/news/add.php';
+    public function createNews()
+    {
+        include __DIR__ . '/../views/Admin/news/add.php';
     }
-    public function handleCreateNews(){
+    public function handleCreateNews()
+    {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if(isset($_POST['btnAddNews'])&&$_POST['btnAddNews']==='Add'){
-                if (isset($_POST['title'], $_POST['content'],$_POST['date'],$_POST['categoryID'],$_FILES['image'])
-                 && !empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['date']) && !empty($_POST['categoryID'])) {
+            if (isset($_POST['btnAddNews']) && $_POST['btnAddNews'] === 'Add') {
+                if (
+                    isset($_POST['title'], $_POST['content'], $_POST['date'], $_POST['categoryID'], $_FILES['image'])
+                    && !empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['date']) && !empty($_POST['categoryID'])
+                ) {
                     $target_dir = "./assets/images/"; //Folder server, where to save image store
                     $target_file = $target_dir . basename($_FILES["image"]["name"]); // Path to file
                     move_uploaded_file($_FILES["image"]["tmp_name"], $target_file); //upload file
@@ -29,25 +30,29 @@ class NewsController
                     $content = $_POST['content'];
                     $date = $_POST['date'];
                     $categoryID = $_POST['categoryID'];
-                    $result = News::addNews($title, $content, $image_url,$date,$categoryID);
+                    $result = News::addNews($title, $content, $image_url, $date, $categoryID);
                     print_r($result);
-                    if($result)
+                    if ($result)
                         header('Location: index.php?controller=News&action=newsManagement');
                 }
             }
         }
     }
     //Update
-    public function updateNews(){
+    public function updateNews()
+    {
         $id = $_GET['id'];
         $news = News::getByID($id);
-        include __DIR__.'/../views/Admin/news/edit.php';
+        include __DIR__ . '/../views/Admin/news/edit.php';
     }
-    public function handleUpdateNews(){
+    public function handleUpdateNews()
+    {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if(isset($_POST['btnUpdateNews'])&&$_POST['btnUpdateNews']==='Update'){
-                if (isset($_POST['title'], $_POST['content'],$_POST['date'],$_POST['categoryID'],$_FILES['image'])
-                 && !empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['date']) && !empty($_POST['categoryID'])) {
+            if (isset($_POST['btnUpdateNews']) && $_POST['btnUpdateNews'] === 'Update') {
+                if (
+                    isset($_POST['title'], $_POST['content'], $_POST['date'], $_POST['categoryID'], $_FILES['image'])
+                    && !empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['date']) && !empty($_POST['categoryID'])
+                ) {
                     $id = $_POST['id'];
                     $title = $_POST['title'];
                     $content = $_POST['content'];
@@ -60,31 +65,32 @@ class NewsController
                         move_uploaded_file($_FILES["image"]["tmp_name"], $target_file); //upload file
                         // Path web display image
                         $image_url = BASE_URL . '/assets/images/' . basename($_FILES["image"]["name"]);
-                    }else{
+                    } else {
                         $news = News::getByID($id);
                         $image_url = $news['image'];
                     }
-                    $result = News::updateNews($title, $content, $image_url,$date,$categoryID,$id);
+                    $result = News::updateNews($title, $content, $image_url, $date, $categoryID, $id);
                     // print_r($result);
-                    if($result)
+                    if ($result)
                         header('Location: index.php?controller=News&action=newsManagement');
-                 }
-                }else header('Location: index.php?controller=News&action=newsManagement');
-            }
+                }
+            } else header('Location: index.php?controller=News&action=newsManagement');
+        }
     }
     //Delete
-    public function deleteNews(){
-        include __DIR__.'/../views/Admin/news/delete.php';
+    public function deleteNews()
+    {
+        include __DIR__ . '/../views/Admin/news/delete.php';
     }
-    public function handleDeleteNews(){
+    public function handleDeleteNews()
+    {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if(isset($_POST['btnDelNews'])&&$_POST['btnDelNews']==='Delete'){
-                    $id = $_POST['id'];
-                    $result = News::deleteNews($id);
-                    if ($result)
+            if (isset($_POST['btnDelNews']) && $_POST['btnDelNews'] === 'Delete') {
+                $id = $_POST['id'];
+                $result = News::deleteNews($id);
+                if ($result)
                     header('Location: index.php?controller=News&action=newsManagement');
-                }
-            else header('Location: index.php?controller=News&action=newsManagement');
-        }   
+            } else header('Location: index.php?controller=News&action=newsManagement');
+        }
     }
 }
